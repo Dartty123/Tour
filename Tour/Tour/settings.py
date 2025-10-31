d1b3364f7471e1befb5a674d9c7c1be4d74a3fda
Dartty123 <dariyatamanuk@icloud.com>
2025-10-31 18:08:48 +0200

    save

--- Tour/Tour/settings.py
+++ Tour/Tour/settings.py
@@ -33,7 +33,10 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'BestTours',
     'UserTour',
+
     'django.contrib.admin',
+ 
+ 
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
